2975a0992cf38517ce02977bd4ce3ecdd1798efe
Xiao Haiping <xiaohp1993@gmail.com>
2018-01-23 16:39:44 +0800

    修改事件处理逻辑，添加注释

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,11 +1,15 @@
 (function() {
     var bindEvents = function() {
         $(document).on('click', '.dropdown', function(e) {
-            $('.dropdown.open').removeClass('open')
-            $(e.currentTarget).addClass('open')
+            // 关闭其他菜单
+            $('.dropdown.open').not(e.currentTarget).removeClass('open')
+            // 切换当前菜单
+            $(e.currentTarget).toggleClass('open')
+
         })
 
         $(document).on('click', function(e) {
+            // 点击空白处关闭
             var menu = $(e.target).closest('.dropdown')
             if (menu.length < 1) {
                 $('.dropdown').removeClass('open')
