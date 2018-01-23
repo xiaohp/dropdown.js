4ebb57fb3ced9acce24c4cb3766921a5e5d3096b
Xiao Haiping <xiaohp1993@gmail.com>
2018-01-23 16:03:42 +0800

    修改为立即执行函数
    菜单绑定改为事件委托

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,11 +1,23 @@
-$('.dropdown').on('click', function(e) {
-    $('.dropdown.open').removeClass('open')
-    $(e.currentTarget).addClass('open')
-})
+(function() {
+    var bindEvents = function() {
+        $(document).on('click', '.dropdown', function(e) {
+            $('.dropdown.open').removeClass('open')
+            $(e.currentTarget).addClass('open')
+        })
 
-$(document).on('click', function(e) {
-    var menu = $(e.target).closest('.dropdown')
-    if (menu.length < 1) {
-        $('.dropdown').removeClass('open')
+        $(document).on('click', function(e) {
+            var menu = $(e.target).closest('.dropdown')
+            if (menu.length < 1) {
+                $('.dropdown').removeClass('open')
+            }
+        })
     }
-})
+
+    var __main = function() {
+        bindEvents()
+    }
+
+    $(document).ready(function() {
+        __main()
+    })
+}())
